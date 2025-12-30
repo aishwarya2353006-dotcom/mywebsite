c6d06d644adc89555d9d5a3ee17c8135182a45be
Aishwarya <aishwarya.2353006@srec.ac.in>
2025-12-30 12:24:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-// Hamburger Menu Toggle
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navbar nav');
-hamburger.addEventListener('click', () => navMenu.classList.toggle('active'));
 
-// Cart Button Counter
-let cartCount = 0;
-document.querySelectorAll('.add-cart').forEach(btn => {
-  btn.addEventListener('click', () => {
-    cartCount++;
-    document.querySelector('.cart-count').textContent = cartCount;
-  });
-});
 
